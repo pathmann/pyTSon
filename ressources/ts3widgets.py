f9d27833a9ed42c950aef53f28e9dd4e39b9ce16
pathmann <thomas.pathmann@googlemail.com>
2017-02-14 02:46:09 +0100

    typo

--- ressources/ts3widgets.py
+++ ressources/ts3widgets.py
@@ -12,7 +12,7 @@ def _errprint(msg, errcode, aid, secid=None):
     if secid:
         err = ts3lib.logMessage("%s (%s): %s" % (msg, secid, errcode), ts3defines.LogLevel.LogLevel_ERROR, "pyTSon.ts3widgets", aid)
     else:
-        err = ts3lib.logMessage("%s: %s" (msg, errcode), ts3defines.LogLevel.LogLevel_ERROR, "pyTSon.ts3widgets", aid)
+        err = ts3lib.logMessage("%s: %s" % (msg, errcode), ts3defines.LogLevel.LogLevel_ERROR, "pyTSon.ts3widgets", aid)
 
     if err != ts3defines.ERROR_ok:
         if secid:
