ba9680a7ec8b8201700ed924ed729b210177efc1
pathmann <thomas.pathmann@googlemail.com>
2017-03-24 13:22:47 +0100

    add translation files

---
+++ generated/pregen/pyTSon.ts
@@ -0,0 +1,447 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<TS language="de_DE" version="2.1">
+  <context>
+    <name>ConfigurationDialog</name>
+    <message>
+      <source>Activation failed</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Changes only take effect after a restart.</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Configure</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Console Background Color</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Console Text Color</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Do you really want to delete plugin {name}?
+This will erase all script data of the plugin from disk.</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Don't ask me again</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error loading iconpack: {exception}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error starting plugin, check your client log for more information</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Remove</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Restart required</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Startup script</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>pyTSon - Settings</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>InstallDialog</name>
+    <message>
+      <source>pyTSon Plugin Installer</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>PluginHost</name>
+    <message>
+      <source>Check for update</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Console</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error calling infoData of python plugin {name}: {trace}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error calling method {methname} in callbackproxy: {trace}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error calling method {methname} of plugin {name}: {trace}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error calling onHotkeyEvent of python plugin {name}: {trace}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error calling onMenuItemEvent of python plugin {name}: {trace}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error calling processCommand of python plugin {name}: {trace}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error checking for update: {errcode}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error loading python plugin from {path}: {trace}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error parsing reply from update check: {trace}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error querying language from client config</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error removing translator</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error starting python plugin {name}: {trace}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error stopping python plugin {name}: {trace}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Settings</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Show the pyTSon settings dialog</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Show the python scripting console</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>There is an update of pyTSon for your platform. Get it from &lt;a href='{url}'&gt;here&lt;/a&gt;</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>You are running the latest pyTSon release</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>You are running the latest pyTSon release (at least for your platform)</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>pyTSon Update Check</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>PluginMount</name>
+    <message>
+      <source>Error loading python plugin {name}, already registered or a plugin with that name already exists</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Plugin {name} not loaded, missing required attribute {attrib}</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>PythonConsole</name>
+    <message>
+      <source>pyTSon Console</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>RepositoryDialog</name>
+    <message>
+      <source>Add repository</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Can't find addon {name} in list</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Can't find repository {name} in list</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Change url of repository {name}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error matching answer from {url} to a repository</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error opening repositorymaster</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error parsing repository {name}: {exception}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Error reading repositorymaster: {trace}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Ignoring online repository {name}, got a local one with that name</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Install</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Internal error</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Invalid entry in repository {name}: {repo}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Invalid entry in repositorymaster</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Invalid repository in list, ignoring</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Name:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Network error updating repository {name}: {error}</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>The URL {url} is not valid</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>This plugin is already installed</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>URL:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Update</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Update available</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Url for repository {name} updated</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Url:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>You have this plugin installed, no update available</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Your local version has a greater version number</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>pyTSon - Online repository</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>pytsonconfigdialog</name>
+    <message>
+      <source>&amp;Description:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>&amp;Keyword:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>&amp;Name:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>&amp;Version:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>A&amp;uthor:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>API &amp;Version:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Accept different API Versions</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Bac&amp;kground:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Colors</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Fa&amp;mily:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Font</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>General</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Get more</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Insert spaces</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Load menuitems from inactive plugins on startup</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Plugins</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Re&amp;quired:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Run script:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Scripting Console</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Si&amp;ze:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Silent</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Startup</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Tabcomplete</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Tabs</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Tabwi&amp;dth:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Te&amp;xt:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Will reload all plugins</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>pytsonui</name>
+    <message>
+      <source>Error loading iconpack: {exception}</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+  <context>
+    <name>repositorydialog</name>
+    <message>
+      <source>Apiversion:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Author:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Description:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Install</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Name:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Reload online repositories</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Repository:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Required Apiversion:</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Update</source>
+      <translation type="unfinished"></translation>
+    </message>
+    <message>
+      <source>Version:</source>
+      <translation type="unfinished"></translation>
+    </message>
+  </context>
+</TS>
