88dfdfcd82ddfd72a8fb8742f657479b40194623
pathmann <thomas.pathmann@googlemail.com>
2017-04-04 17:25:04 +0200

    file info now all displayed

--- ressources/python/ts3widgets/filetransfer.py
+++ ressources/python/ts3widgets/filetransfer.py
@@ -6,13 +6,42 @@ import ts3lib
 from ts3defines import ERROR_ok, ERROR_database_empty_result, FileListType
 import pytson
 from pytsonui import setupUi
+import ts3client
 
 from pluginhost import PluginHost
 from signalslot import Signal
 
 from PythonQt.QtCore import (Qt, QAbstractItemModel, QModelIndex)
-from PythonQt.QtGui import (QDialog, QStyledItemDelegate, QIcon,
+from PythonQt.QtGui import (QDialog, QStyledItemDelegate, QIcon, QHeaderView,
                             QSortFilterProxyModel)
+
+from datetime import datetime
+
+
+def splitpath(path):
+    """
+
+    """
+    return list(filter(None, path.split('/')))
+
+
+def joinpath(*args):
+    """
+
+    """
+    return "/" + "/".join(filter(None, *args))
+
+
+def bytesToStr(size):
+    bias = 1024.0
+    units = ['B', 'KiB', 'MiB', 'GiB', 'TiB']
+
+    for u in units:
+        if abs(size) < bias:
+            return "%3.2f %s" % (size, u)
+        size /= bias
+
+    return "%3.2f PiB" % size
 
 
 class File(object):
@@ -20,20 +49,26 @@ class File(object):
 
     """
 
-    def __init__(self, path, name, size, datetime, atype, incompletesize):
+    def __init__(self, path, name, size, date, atype, incompletesize):
         self.path = path
         self.name = name
         self.size = size
-        self.datetime = datetime
+        self.datetime = datetime.fromtimestamp(date)
         self.type = atype
         self.incompletesize = incompletesize
 
     @property
     def isDirectory(self):
+        """
+
+        """
         return self.type == FileListType.FileListType_Directory
 
     @property
     def icon(self):
+        """
+
+        """
         if self.isDirectory:
             return QIcon.fromTheme("folder")
         else:
@@ -63,8 +98,6 @@ class FileListModel(QAbstractItemModel, pytson.Translatable):
 
     """
 
-    titles = ["Name"]
-
     def __init__(self, schid, cid, password, parent=None):
         """
 
@@ -81,6 +114,9 @@ class FileListModel(QAbstractItemModel, pytson.Translatable):
         self.newpath = None
         self.files = []
         self.newfiles = []
+
+        self.titles = [self._tr("Name"), self._tr("Size"), self._tr("Type"),
+                       self._tr("Last Changed")]
 
         PluginHost.registerCallbackProxy(self)
 
@@ -111,13 +147,13 @@ class FileListModel(QAbstractItemModel, pytson.Translatable):
             _errprint(self._tr("Error requesting filellist"), err, self.schid,
                       self.cid)
 
-    def onFileListEvent(self, schid, channelID, path, name, size, datetime,
+    def onFileListEvent(self, schid, channelID, path, name, size, date,
                         atype, incompletesize, returnCode):
         if (schid != self.schid or channelID != self.cid or
            returnCode != self.retcode):
             return
 
-        self.newfiles.append(File(path, name, size, datetime, atype,
+        self.newfiles.append(File(path, name, size, date, atype,
                                   incompletesize))
 
     def onFileListFinishedEvent(self, schid, channelID, path):
@@ -175,27 +211,37 @@ class FileListModel(QAbstractItemModel, pytson.Translatable):
                 return f.name
             elif role == Qt.DecorationRole:
                 return f.icon
-
-        if role == Qt.UserRole:
-            if f.isDirectory:
-                return "a%s" % f.name
-            else:
-                return "b%s" % f.name
-
+            elif role == Qt.UserRole:
+                if f.isDirectory:
+                    return "a%s" % f.name
+                else:
+                    return "b%s" % f.name
+        elif role == Qt.DisplayRole:
+            if idx.column() == 1 and not f.isDirectory:
+                return bytesToStr(f.size)
+            elif idx.column() == 2:
+                if f.isDirectory:
+                    return self._tr("Directory")
+                else:
+                    return self._tr("File")
+            elif idx.column() == 3:
+                return f.datetime.strftime(pytson.tr("filetransfer",
+                                                     "%Y-%m-%d %H:%M:%S"))
         return None
 
 
-class FileBrowser(QDialog):
+class FileBrowser(QDialog, pytson.Translatable):
     """
 
     """
 
-    def __init__(self, schid, cid, password, path, parent=None, *,
-                 staticpath=False, readonly=False, iconpack=None):
+    def __init__(self, schid, cid, password='', path='/', parent=None, *,
+                 staticpath=False, readonly=False, downloaddir=None,
+                 iconpack=None):
         """
 
         """
-        super().__init__(parent)
+        super(QDialog, self).__init__(parent)
         self.setAttribute(Qt.WA_DeleteOnClose)
 
         try:
@@ -205,16 +251,26 @@ class FileBrowser(QDialog):
             self.delete()
             raise e
 
-        self.filterFrame.hide()
-
-        self.iconButton.setChecked(True)
-        self.stack.setCurrentWidget(self.listPage)
-
         self.cid = cid
         self.password = password
+        self.path = None
 
         self.staticpath = staticpath
         self.readonly = readonly
+
+        if not self.readonly and not downloaddir:
+            cfg = ts3client.Config()
+            q = cfg.query("SELECT value FROM filetransfer "
+                          "WHERE key='DownloadDir'")
+            del cfg
+
+            if q.next():
+                self.downloaddir = q.value("value")
+            else:
+                self.delete()
+                raise Exception("Error getting DownloadDir from config")
+        else:
+            self.downloaddir = downloaddir
 
         self.listmodel = FileListModel(schid, cid, password, self)
         self.listmodel.pathChanged.connect(self.onPathChanged)
@@ -227,12 +283,43 @@ class FileBrowser(QDialog):
 
         self.listmodel.path = path
 
+        self.adjustUi()
+
+    def adjustUi(self):
+        """
+
+        """
+        self.filterFrame.hide()
+
+        self.iconButton.setChecked(True)
+        self.stack.setCurrentWidget(self.listPage)
+
         self.list.setModel(self.proxy)
         self.table.setModel(self.proxy)
         self.table.sortByColumn(0, Qt.AscendingOrder)
 
+        if self.staticpath:
+            self.upButton.setEnabled(False)
+            self.homeButton.setEnabled(False)
+
+        if self.readonly:
+            self.uploadButton.setEnabled(False)
+            self.downloadButton.setEnabled(False)
+            self.directoryButton.setEnabled(False)
+            self.deleteButton.setEnabled(False)
+
+        header = self.table.horizontalHeader()
+        header.setSectionResizeMode(0, QHeaderView.Stretch)
+        for i in range(1, header.count()):
+            header.setSectionResizeMode(i, QHeaderView.ResizeToContents)
+
     def onPathChanged(self, newpath):
+        self.path = newpath
         self.pathEdit.setText(newpath)
+
+        inroot = newpath == "/"
+        self.upButton.setEnabled(not inroot)
+        self.homeButton.setEnabled(not inroot)
 
     def on_pathEdit_returnPressed(self):
         oldpath = self.listmodel.path
@@ -258,6 +345,55 @@ class FileBrowser(QDialog):
 
     def on_filterEdit_textChanged(self, newtext):
         self.proxy.setFilterRegExp(newtext)
+
+    def on_upButton_clicked(self):
+        if self.staticpath:
+            return
+        if self.path == "/":
+            return
+
+        self.listmodel.path = joinpath(splitpath(self.path)[:-1])
+
+    def on_homeButton_clicked(self):
+        if self.staticpath:
+            return
+
+        self.listmodel.path = "/"
+
+    def on_refreshButton_clicked(self):
+        self.listmodel.path = self.listmodel.path
+
+    def on_uploadButton_clicked(self):
+        if self.readonly:
+            return
+
+        #TODO
+
+    def on_downloadButton_clicked(self):
+        if self.readonly:
+            return
+
+        #TODO
+
+    def on_directoryButton_clicked(self):
+        if self.readonly:
+            return
+
+        #TODO
+
+    def on_deleteButton_clicked(self):
+        if self.readonly:
+            return
+
+        #TODO
+
+    def on_table_customContextMenuRequested(self, pos):
+        #TODO
+        pass
+
+    def on_list_customContextMenuRequested(self, pos):
+        #TODO
+        pass
 
 
 class FileCollisionAction(object):
