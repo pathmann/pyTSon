81acd11359d3c1d893063ed307e7b029d31c078d
pathmann <thomas.pathmann@googlemail.com>
2017-04-27 18:22:59 +0200

    add decorator for QLayout::getContentsMargin

--- shared/pythonqt/pythonqtpytson.cpp
+++ shared/pythonqt/pythonqtpytson.cpp
@@ -1,1 +1,10 @@
 #include "pythonqtpytson.h"
+
+#include <QLayout>
+
+const QVariantList pytsondecorator::getContentsMargins(QLayout* layout) {
+  int top, left, bottom, right;
+  layout->getContentsMargins(&left, &top, &right, &bottom);
+
+  return QVariantList() << left << top << right << bottom;
+}
--- shared/pythonqt/pythonqtpytson.h
+++ shared/pythonqt/pythonqtpytson.h
@@ -3,8 +3,11 @@
 
 #include <QObject>
 
+#include <QVariantList>
+
 #include "eventfilterobject.h"
 
+class QLayout;
 
 class pytsondecorator: public QObject {
   Q_OBJECT
@@ -12,6 +15,8 @@ class pytsondecorator: public QObject {
   public slots:
     EventFilterObject* new_EventFilterObject(const QList<int>& types = QList<int>(), QObject* parent = 0) { return new EventFilterObject(types, parent); }
     void delete_EventFilterObject(EventFilterObject* obj) { delete obj; }
+
+    const QVariantList getContentsMargins(QLayout* layout);
 };
 
 #endif // PYTHONQTPYTSON_H
